810f60fa1439e550200b9c6c3500fb980967c79e
Jordanpatton78 <44303813+Jordanpatton78@users.noreply.github.com>
2019-02-03 20:22:14 -0700

    Update MainActivity2.java

--- app/java/MainActivity2.java
+++ app/java/MainActivity2.java
@@ -1,6 +1,14 @@
-<?xml version="1.0" encoding="utf-8"?>
-<android.support.constraint.ConstraintLayout
-    xmlns:android="http://schemas.android.com/apk/res/android" android:layout_width="match_parent"
-    android:layout_height="match_parent">
+package com.example.jordan.physicshelper;
 
-</android.support.constraint.ConstraintLayout>
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
