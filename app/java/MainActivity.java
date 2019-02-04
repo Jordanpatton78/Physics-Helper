664537483e641a1474cf7f8789ac33e73132e59f
Jordanpatton78 <44303813+Jordanpatton78@users.noreply.github.com>
2019-02-03 20:21:02 -0700

    Update MainActivity.java

--- app/java/MainActivity.java
+++ app/java/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button velocity = (Button) findViewById(R.id.velocity);
+        Button velocity = findViewById(R.id.velocity);
         velocity.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent myIntent= new Intent(v.getContext(),velocity.class);
@@ -24,6 +24,52 @@ public class MainActivity extends AppCompatActivity {
                                     }
 
         );
-
+        final Button acceleration=findViewById(R.id.acceleration);
+        acceleration.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent myIntent =new Intent(v.getContext(),acceleration.class);
+                startActivityForResult(myIntent,0);
+            }
+        });
+        Button ke1=findViewById(R.id.ke1);
+        ke1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent myIntent=new Intent(v.getContext(),ke1.class);
+                startActivityForResult(myIntent,0);
+            }
+        });
+        Button ke2=findViewById(R.id.ke2);
+        ke2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent=new Intent(v.getContext(),ke2.class);
+                startActivityForResult(myIntent,0);
+            }
+        });
+        Button ke3=findViewById(R.id.ke3);
+        ke3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent=new Intent(v.getContext(),ke3.class);
+                startActivityForResult(myIntent,0);
+            }
+        });
+        Button ke4=findViewById(R.id.ke4);
+        ke4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent=new Intent(v.getContext(),ke4.class);
+                startActivityForResult(myIntent,0);
+            }
+        });
+        Button more=findViewById(R.id.more);
+        more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent=new Intent(v.getContext(),MainActivity2.class);
+                startActivityForResult(myIntent,0);
+            }
+        });
     }
 }
