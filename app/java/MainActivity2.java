1d750e1937f7aadfdc5767e4e81bbcd2fadb12e5
Jordanpatton78 <44303813+Jordanpatton78@users.noreply.github.com>
2019-02-19 22:06:48 -0700

    Update MainActivity2.java

--- app/java/MainActivity2.java
+++ app/java/MainActivity2.java
@@ -1,7 +1,10 @@
 package com.example.jordan.physicshelper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        Button work=findViewById(R.id.work);
+        work.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent=new Intent(v.getContext(),work.class);
+                startActivityForResult(myIntent,0);
+            }
+        });
+        final Button consOfMomentum=findViewById(R.id.momentum);
+        consOfMomentum.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent myIntent=new Intent(v.getContext(), momentum.class);
+                startActivityForResult(myIntent,0);
+
+            }
+        });
     }
 }
